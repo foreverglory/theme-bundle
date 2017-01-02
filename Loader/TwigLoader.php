e70e65b1acad6eba72c2312e1722e52313e52eba
ForeverGlory <foreverglory@qq.com>
2017-01-02 16:22:55 +0800

    twig loader

--- Loader/TwigLoader.php
+++ Loader/TwigLoader.php
@@ -12,10 +12,42 @@ use Symfony\Bundle\TwigBundle\Loader\FilesystemLoader;
 class TwigLoader extends FilesystemLoader
 {
 
+    /**
+     * Adds a path where templates are stored.
+     *
+     * @param string $path      A path where to look for templates
+     * @param string $namespace A path name
+     *
+     * @throws Twig_Error_Loader
+     */
+    public function addPath($path, $namespace = self::MAIN_NAMESPACE)
+    {
+        if (in_array($namespace, [])) {
+            //增加路径
+        }
+        parent::addPath($path, $namespace);
+    }
+
     protected function findTemplate($template, $throw = true)
     {
         //todo: find formatPath Template first
         return parent::findTemplate($template, $throw);
+    }
+
+    protected function parseName($name, $default = self::MAIN_NAMESPACE)
+    {
+        if (isset($name[0]) && '@' == $name[0]) {
+            if (false === $pos = strpos($name, '/')) {
+                throw new Twig_Error_Loader(sprintf('Malformed namespaced template name "%s" (expecting "@namespace/template_name").', $name));
+            }
+            //todo: 这里将替换模板 $namespace, $shortname;
+            $namespace = substr($name, 1, $pos - 1);
+            $shortname = substr($name, $pos + 1);
+            $shortname = 'demo/demo.html.twig';
+            return array($namespace, $shortname);
+        }
+
+        return array($default, $name);
     }
 
 }
